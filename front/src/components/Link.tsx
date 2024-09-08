1c8830ac27df3efd0c7beb5e74a2bc02d8786710
feyleth <feyleth@gmail.com>
2024-09-08 15:51:56 +0200

    start curve line

--- front/src/components/Link.tsx
+++ front/src/components/Link.tsx
@@ -1,3 +1,4 @@
+import { createMemo } from "solid-js";
 
 
 export type LinkType = { from: { top: number, left: number }, to: { top: number, left: number } };
@@ -6,6 +7,11 @@ type Props = {
 }
 export default function(props: Props) {
 
-    return (<path d={`M ${props.link.from.left} ${props.link.from.top} L ${props.link.to.left} ${props.link.to.top}`} stroke="black" stroke-width="2px" />)
+    let middle = createMemo(() => {
+        let link = props.link;
+        return { left: (link.to.left - link.from.left) / 2, top: (link.to.top - link.from.top) / 2 }
+    })
+
+    return (<path d={`M ${props.link.from.left} ${props.link.from.top} q ${middle().left} 0 ${middle().left} ${middle().top} T ${props.link.to.left} ${props.link.to.top}`} stroke="black" stroke-width="2px" fill="transparent" />)
 
 }
